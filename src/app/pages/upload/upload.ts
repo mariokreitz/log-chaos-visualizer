c0a84860ca7aee890d0352fa154a82532d8391e7
mariokreitz <mario.kreitz@web.de>
2025-12-04 13:13:30 +0100

    feat(upload): add upload component with placeholder content

---
+++ src/app/pages/upload/upload.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-upload',
+    imports: [],
+    templateUrl: './upload.html',
+    styleUrl: './upload.scss',
+})
+export default class Upload {
+}
