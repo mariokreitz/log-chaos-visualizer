fb1c30c99240ff8a9b51c4d119ceafa572e1263d
mariokreitz <mario.kreitz@web.de>
2025-12-04 15:32:22 +0100

    refactor(analyse): remove unused file parsing logic

--- src/app/pages/analyse/analyse.ts
+++ src/app/pages/analyse/analyse.ts
@@ -1,33 +1,13 @@
-import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
-import { FileSelector } from '../../core/components/file-selector/file-selector';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { APP_CONFIG } from '../../core/config/app-config';
-import { FileParseService } from '../../core/services/file-parse.service';
 
 @Component({
     selector: 'app-analyse',
-    imports: [ FileSelector ],
+    imports: [],
     templateUrl: './analyse.html',
     styleUrl: './analyse.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class Analyse {
     readonly featureFlags = APP_CONFIG.featureFlags;
-
-    private readonly parse = inject(FileParseService);
-
-    readonly progress = this.parse.progress;
-    readonly summary = this.parse.summary;
-    readonly error = this.parse.error;
-    readonly isParsing = this.parse.isParsing;
-    readonly latestBatch = this.parse.latestBatch;
-
-    readonly hasFile = computed(() => this.parse.selectedFile() !== null);
-
-    onFileSelected(file: File): void {
-        this.parse.setFile(file);
-    }
-
-    startParse(): void {
-        this.parse.startParse();
-    }
 }
