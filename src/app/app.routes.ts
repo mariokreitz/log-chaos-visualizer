52f2b18a78e555390682fe2ca3c6f2e6837c1701
mariokreitz <mario.kreitz@web.de>
2025-12-04 13:11:17 +0100

    feat(routes): restructure routing to include layout and child routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,20 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: '',
-        loadComponent: () => import('./pages/dashboard/dashboard'),
+        loadComponent: () => import('./core/layout/layout').then(m => m.Layout),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./pages/dashboard/dashboard'),
+            },
+            {
+                path: 'upload',
+                loadComponent: () => import('./pages/upload/upload'),
+            },
+            {
+                path: 'analyse',
+                loadComponent: () => import('./pages/analyse/analyse'),
+            },
+        ],
     },
 ];
