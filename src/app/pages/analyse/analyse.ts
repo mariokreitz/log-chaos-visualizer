eec0797f7cafc16badc425adc31512e4e92ad908
mariokreitz <mario.kreitz@web.de>
2025-12-04 22:43:25 +0100

    refactor(analyse): remove unused ExperimentalLogTable import

--- src/app/pages/analyse/analyse.ts
+++ src/app/pages/analyse/analyse.ts
@@ -1,10 +1,9 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FeatureFlagsService } from '../../core/services/feature-flags.service';
-import { ExperimentalLogTable } from './experimental-log-table/experimental-log-table';
 
 @Component({
   selector: 'app-analyse',
-  imports: [ExperimentalLogTable],
+  imports: [],
   templateUrl: './analyse.html',
   styleUrl: './analyse.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
