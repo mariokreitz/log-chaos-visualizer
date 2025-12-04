3180eeae8bef1cd13ded5470f2e4c865f2241954
mariokreitz <mario.kreitz@web.de>
2025-12-04 13:11:12 +0100

    feat(analyse): add analyse component with template and styles

---
+++ src/app/pages/analyse/analyse.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-analyse',
+    imports: [],
+    templateUrl: './analyse.html',
+    styleUrl: './analyse.scss',
+})
+export default class Analyse {
+}
