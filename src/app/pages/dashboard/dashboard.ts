90bac403388e51ae14e741705deb31a6585fdd9a
mariokreitz <mario.kreitz@web.de>
2025-12-04 13:11:26 +0100

    refactor(dashboard): simplify layout and header structure

--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { Layout } from '../../core/layout/layout';
 
 @Component({
     selector: 'app-dashboard',
-    imports: [
-        Layout,
-    ],
+    imports: [],
     templateUrl: './dashboard.html',
     styleUrl: './dashboard.scss',
 })
