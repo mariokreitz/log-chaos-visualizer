e9c61521396013a32155154135b748532735092f
mariokreitz <mario.kreitz@web.de>
2025-12-04 12:27:52 +0100

    feat(dashboard): add dashboard component and routing

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Layout } from '../../core/layout/layout';
+
+@Component({
+  selector: 'app-dashboard',
+    imports: [
+        Layout,
+    ],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.scss',
+})
+export default class Dashboard {
+
+}
