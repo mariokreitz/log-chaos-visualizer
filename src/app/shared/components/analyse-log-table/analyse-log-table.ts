b067ec71435a28631bdc0f0e74c2833e9a7bb362
mariokreitz <mario.kreitz@web.de>
2025-12-05 20:13:48 +0100

    feat(analyse-log-table): add initial component structure

---
+++ src/app/shared/components/analyse-log-table/analyse-log-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analyse-log-table',
+  imports: [],
+  templateUrl: './analyse-log-table.html',
+  styleUrl: './analyse-log-table.scss',
+})
+export class AnalyseLogTable {
+
+}
