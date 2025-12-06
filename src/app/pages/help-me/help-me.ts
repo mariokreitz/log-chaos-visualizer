d0fca11b033826e08f8a9d531dc34116133a9290
mariokreitz <mario.kreitz@web.de>
2025-12-06 01:05:42 +0100

    feat(help-me): add help component with basic template

---
+++ src/app/pages/help-me/help-me.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-help-me',
+  imports: [],
+  templateUrl: './help-me.html',
+  styleUrl: './help-me.scss',
+})
+export default class HelpMe {}
