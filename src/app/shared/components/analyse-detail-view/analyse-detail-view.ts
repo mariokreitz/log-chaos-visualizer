678d91b35dbb1775c511654c29d1d54f483d59f7
mariokreitz <mario.kreitz@web.de>
2025-12-07 13:46:02 +0100

    feat(analyse-detail-view): add initial component scaffold

---
+++ src/app/shared/components/analyse-detail-view/analyse-detail-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analyse-detail-view',
+  imports: [],
+  templateUrl: './analyse-detail-view.html',
+  styleUrl: './analyse-detail-view.scss',
+})
+export class AnalyseDetailView {
+
+}
